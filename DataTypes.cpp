d5a6a1e2a1115767f6cc27220f032cc6031c7281
Gaurav Kumar <142656078+Gaurav-K-Github@users.noreply.github.com>
2025-03-25 23:45:33 +0530

    Add files via upload

---
+++ DataTypes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int v1=5;
+    double v2=5.9;
+    char v3 = 'A';
+    string v4="abc";
+    bool v5=true;
+
+    //this is how a comment is written
+
+    cout<<v1<<"::integer\n";
+    cout<<v2<<"::float\n";
+    cout<<v3<<"::character\n";
+    cout<<v4<<"::string\n";
+    cout<<v5<<"::boolean\n";
+
+}
